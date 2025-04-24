ccf322369a9fc5a9f94913b538fbeb35fa506449
HardlyCodeMan <72480678+HardlyCodeMan@users.noreply.github.com>
2025-04-24 11:23:25 +0800

    Add: HTMLNode and tests

---
+++ src/htmlnode.py
@@ -0,0 +1,24 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        props_str = []
+        for key, value in self.props.items():
+            if isinstance(value, str):
+                props_str.append(f'{key}="{value}"')
+            else:
+                props_str.append(f'{key}={value}')
+        return " ".join(props_str)
+
+    def __repr__(self):
+        out = f"TextNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return out
---
+++ src/test_htmlnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHtmlNode(unittest.TestCase):
+    def test_to_html(self):
+        node = HTMLNode(tag="div", value="Hello, World!", children=[], props={"class": "greeting"})
+        #expected_html = '<div class="greeting">Hello, World!</div>'
+        with self.assertRaises(NotImplementedError):
+            node.to_html()
+        #self.assertEqual(node.to_html(), expected_html)
+    
+    def test_props_to_html(self):
+        node = HTMLNode(tag="div", value="Hello, World!", children=[], props={"class": "greeting"})
+        expected_props = 'class="greeting"'
+        self.assertEqual(node.props_to_html(), expected_props)
+    
+    def test_repr(self):
+        node = HTMLNode(tag="div", value="Hello, World!", children=[], props={"class": "greeting"})
+        expected_repr = 'TextNode(div, Hello, World!, [], {\'class\': \'greeting\'})'
+        self.assertEqual(repr(node), expected_repr)
+
+if __name__ == "__main__":
+    unittest.main()
